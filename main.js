7e19702b6b50821f48648ef00ac2b4886d9534e1
Leandro Silva <leandro.csilva356@gmail.com>
2023-10-03 04:09:26 -0300

    fix: limitação de casas decimais

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ function adicionaLinha() {
 
     linhas += linha;
     }
-    
+
     inputNomeAtividade.value = '';
     inputNotaAtividade.value = '';
 }
@@ -48,7 +48,7 @@ function atualizaTabela() {
 function atualizaMediaFinal() {
     const mediaFinal = calculaMediaFinal();
 
-    document.getElementById('media-final-valor').innerHTML = mediaFinal;
+    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2);
     document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 }
 
